44532d61665abbc5939ef991c1c60cfd1a8be99f
zeropaul <landerspaulzero@gmail.com>
2016-10-15 16:13:42 -0500

    Hero ZI

---
+++ app_name/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class {{ app_name.capitalize }}Config(AppConfig):
+
+    name = 'apps.{{ app_name }}'
---
+++ app_name/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+app_name = '{{ app_name }}'
+
+urlpatterns = [
+    #url(r'^{{ app_name }}/', namespace='app_{{ app_name }}' ),
+
+]
